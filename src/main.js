b3d4d23fc991a8d0b19919c1b1590d29fb74abb1
Monica Yamazaki <monicasayama@gmail.com>
2020-03-09 19:43:05 -0300

    nav btn + css filter-egg e filter-sort

--- src/main.js
+++ src/main.js
@@ -1,21 +1,48 @@
 // calculadora
-
-const resultado = document.getElementById("resultado");
-
 const calculo = (velocidade, ovo, incubadora, tempoDiario) => {
-  resultado.innerHTML = `<p>Você irá caminhar por aproximadamente:  ${calculadora(velocidade, ovo, incubadora, tempoDiario)} dias`;
+  const resultado = document.getElementById("resultado");
+  resultado.innerHTML = `<p>Aproximadamente ${calculadora(
+    velocidade,
+    ovo,
+    incubadora,
+    tempoDiario
+  )} dias`;
 };
 
-document.getElementById("calcular").addEventListener("click", () => {
+const tablink = document.querySelectorAll(".tablink");
+tablink.forEach(tab => {
+  tab.addEventListener("click", e => {
+    removeAllContent();
+    showCurrentTab(e);
+  });
+});
+
+const removeAllContent = () => {
+  const tabcontent = document.querySelectorAll(".tabcontent");
+  tabcontent.forEach(content => {
+    content.classList.remove("show-tab");
+  });
+};
+
+const showCurrentTab = e => {
+  document
+    .getElementById(e.currentTarget.dataset.page)
+    .classList.add("show-tab");
+};
+
+document.getElementById("calcular").addEventListener("click", event => {
   event.preventDefault();
-  const velocidade = document.querySelector("input[name =\"velocidade\"]:checked").value;
-  const ovo = document.querySelector("input[name=\"ovo\"]:checked").value;
-  const incubadora = document.querySelector("input[name=\"incubadora\"]:checked").value;
+  const velocidade = document.querySelector("input[name ='velocidade']:checked")
+    .value;
+  const ovo = document.querySelector("input[name='ovo']:checked").value;
+  const incubadora = document.querySelector(
+    "input[name='calc-incubadora']:checked"
+  ).value;
   const tempoDiario = document.getElementById("tempo-diario").value;
   if (+tempoDiario <= 0) {
     alert("Insira um tempo válido");
   } else {
-  };
+  }
   calculo(velocidade, ovo, incubadora, tempoDiario);
 });
 
@@ -29,8 +56,10 @@ radio = document.getElementById("radio");
 radio.addEventListener("change", () => {
   filtrado = filtro(listapokemon, event.target.value);
   printar(filtrado, printarpoke);
-  let porcent = porcentagem(filtrado, listapokemon);
-  printarPorcentagem.innerHTML = `SABIA? Com o ovo de ${event.target.value}, você poderá eclodir ${porcent.toFixed(2)}% dos 150 pokemons de Kanto!`;
+  // let porcent = porcentagem(filtrado, listapokemon);
+  // printarPorcentagem.innerHTML = `SABIA? Com o ovo de ${
+  //   event.target.value
+  // }, você poderá eclodir ${porcent.toFixed(2)}% dos 150 pokemons de Kanto!`;
 });
 
 // ordenação 150 pokemons
@@ -49,27 +78,25 @@ const printar = (array, element) => {
   element.innerHTML = "";
   array.forEach(element => {
     layout += `
-    <div class="card">
-      <div class="card-pokemon">
+      <div class="card-pokemon border">
         <img src = "${element.img}">
-        <span class="name"> ${element.id}. ${element.name}</span>
-        <p class="type"> Tipo: ${element.type.join(" e ")}</p>
-      </div>
-    </div>`;
+        <p class="name margin-none"> ${element.id}. ${element.name}</p>
+        <p class="type margin-none"> Tipo: ${element.type.join(" e ")}</p>
+      </div>`;
   });
   element.innerHTML = layout;
 };
 
 // menu
 
-openMenu = document.getElementById("openMenu").addEventListener("click", () => {
-  event.preventDefault();
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
-});
+// openMenu = document.getElementById("openMenu").addEventListener("click", () => {
+//   event.preventDefault();
+//   var x = document.getElementById("myTopnav");
+//   if (x.className === "topnav") {
+//     x.className += " responsive";
+//   } else {
+//     x.className = "topnav";
+//   }
+// });
 
 window.onload = printar(listapokemon, showpokemons);
